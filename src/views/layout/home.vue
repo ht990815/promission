a2f98760062f0a6e5c9c6c6eeef9fe5e9798d039
Your Name <you@example.com>
2022-02-08 21:42:03 +0800

    搭建

---
+++ src/views/layout/home.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        background-color="black"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="2">
+          <router-link to="/saijia">
+            <i class="el-icon-menu"></i>
+            <span slot="title">赛迦</span>  
+          </router-link>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">奥特之王</span>
+        </el-menu-item>
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>奥特曼</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-container {
+    height: 100%;
+  }
+
+  .el-menu {
+    height: 100%;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 100%;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 100%;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 100%;
+  }
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='loginbox'>
+    <el-row style="background:gray;padding:50px;border-radius:10px;">
+      <el-row>
+        <el-row>
+          <span>账号：</span><el-input v-model='form.acc' style="width:200px;"></el-input>
+        </el-row>
+        <el-row>
+          <span>密码：</span><el-input v-model='form.pwd' style="width:200px;" type="password"></el-input>
+        </el-row>
+      </el-row>
+      <el-row>
+        <el-button @click="Login">登录</el-button>
+        <el-button type="primary">重置</el-button>
+      </el-row>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        acc: '',
+        pwd: ''
+      }
+    }
+  },
+  methods: {
+    Login() {
+      this.$router.push('/home');
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  body .loginbox {
+    width: 100%;
+    height: 100%;
+    background-color: rgb(24, 23, 23);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color:white;
+ }
+</style>
